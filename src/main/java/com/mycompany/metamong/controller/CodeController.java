6487a7b8f87d36edc5ab32acfc8bb540baf97dd1
soob511 <asdfls@naver.com>
2024-10-21 20:56:07 +0900

    Feat: 코드/항목 css 추가

--- src/main/java/com/mycompany/metamong/controller/CodeController.java
+++ src/main/java/com/mycompany/metamong/controller/CodeController.java
@@ -10,8 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/code")
 public class CodeController {
-	@GetMapping("")
+	@GetMapping("/codeList")
 	public String navToApplyCode() {
-		return "code/applyCode";
+		return "code/codeList";
 	}
 }
