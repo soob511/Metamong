bc15a90a4c6a7b866f4858b1ab9207aa663dd601
gilju <giljurha93@gmail.com>
2024-10-21 13:33:55 +0900

    Feat: 코드 컨트롤러 및 JSP 생성

---
+++ src/main/java/com/mycompany/metamong/controller/CodeController.java
@@ -0,0 +1,17 @@
+package com.mycompany.metamong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/code")
+public class CodeController {
+	@GetMapping("")
+	public String navToApplyCode() {
+		return "code/applyCode";
+	}
+}
