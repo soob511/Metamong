a8b4425b19de7fb0157972c896040aaf75326136
soob511 <asdfls@naver.com>
2024-10-21 11:34:18 +0900

    Update: 회원가입 페이지 css완성

---
+++ src/main/java/com/mycompany/metamong/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.mycompany.metamong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "member/joinForm";
+	}
+}
