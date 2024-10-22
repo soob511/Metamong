955d7f97811eee2b41b157dca79041473bb01899
soob511 <asdfls@naver.com>
2024-10-22 16:04:30 +0900

    Feat: 로그추가

--- src/main/java/com/mycompany/metamong/controller/HomeController.java
+++ src/main/java/com/mycompany/metamong/controller/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HomeController {
 
 	@RequestMapping("")
@@ -14,6 +17,7 @@ public class HomeController {
 	
 	@GetMapping("/home")
 	public String home() {
+		log.info("실행");
 		return "home";
 	}
 }
