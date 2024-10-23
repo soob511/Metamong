a583e5254ddb2505c23294a26284341d72454a5a
gilju <giljurha93@gmail.com>
2024-10-23 21:16:32 +0900

    Feat: Member dto 생성 및 dao 구현

--- src/main/java/com/mycompany/metamong/dao/MemberDao.java
+++ src/main/java/com/mycompany/metamong/dao/MemberDao.java
@@ -2,7 +2,11 @@ package com.mycompany.metamong.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.mycompany.metamong.dto.MemberDto;
+
 @Mapper
 public interface MemberDao {
-
+	
+	public MemberDto selectByMId(String mId);
+	
 }
--- src/main/java/com/mycompany/metamong/dto/MemberDto.java
+++ src/main/java/com/mycompany/metamong/dto/MemberDto.java
@@ -1,8 +1,23 @@
 package com.mycompany.metamong.dto;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class MemberDto {
-
+	private String MId;
+	private String MName;
+	private String MPassword;
+	private int teamId;
+	private String MEmpId;
+	private int MTel;
+	private String MRole;
+	private boolean MIsActive;
+	private String profFilename;
+	private String profFiletype;
+	private byte[] profFiledata;
+	private Date MApplydate;
+	private boolean MIsapprove;
+	private Date MRegdate;
 }
