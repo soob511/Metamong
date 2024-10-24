212b2c700bf9b22048c6146d8931d6f17878c185
gilju <giljurha93@gmail.com>
2024-10-24 15:19:46 +0900

    Refactor: 홈 컨트롤러 띄어쓰기를 자꾸 깃에서 잡아서 불가피하게 커밋

--- src/main/java/com/mycompany/metamong/controller/HomeController.java
+++ src/main/java/com/mycompany/metamong/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.mycompany.metamong.controller;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ public class HomeController {
 	public String index() {
 		return "member/loginForm";
 	}
-	
+
 	@GetMapping("/homeUser")
 	public String homeUser() {
 		log.info("실행");
