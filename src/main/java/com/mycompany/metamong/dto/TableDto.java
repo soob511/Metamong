c1b4604b6824a93abe474e9d58c0d10fed61f869
soob511 <asdfls@naver.com>
2024-10-26 18:43:27 +0900

    Feat: 테이블 조회 기능 추가

---
+++ src/main/java/com/mycompany/metamong/dto/TableDto.java
@@ -0,0 +1,17 @@
+package com.mycompany.metamong.dto;
+
+import lombok.Data;
+
+@Data
+public class TableDto {
+
+	private int  tableNo;
+	
+	private String tableNm;
+	
+	private String tableId;
+	
+	private String tableContent;
+	
+	private String schemaNm;
+}
---
+++ src/main/java/com/mycompany/metamong/service/TableService.java
@@ -0,0 +1,24 @@
+package com.mycompany.metamong.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mycompany.metamong.daoMain.TableDao;
+import com.mycompany.metamong.dto.TableDto;
+
+@Service
+public class TableService {
+	
+	@Autowired
+	private TableDao tableDao;
+
+	public List<TableDto> getTableList() {
+		List<TableDto> list = tableDao.selectTableList();
+		System.out.println(list);
+		return list;
+/*		return tableDao.selectTableList();*/
+	}
+
+}
