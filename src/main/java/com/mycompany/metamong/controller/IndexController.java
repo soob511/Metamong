bc3fa9aac1372366faf96772dd97dcbb8f741864
gilju <giljurha93@gmail.com>
2024-10-26 16:19:22 +0900

    Feat: 컨트롤러 searchIndex 메서드 추가 및 JSP 수정

--- src/main/java/com/mycompany/metamong/controller/IndexController.java
+++ src/main/java/com/mycompany/metamong/controller/IndexController.java
@@ -1,16 +1,50 @@
 package com.mycompany.metamong.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mycompany.metamong.dto.IndexDto;
+import com.mycompany.metamong.enums.SchemaEnums;
+import com.mycompany.metamong.service.IndexService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/index")
 public class IndexController {
+	@Autowired
+	private IndexService indexService;
 	
 	@GetMapping("/indexList")
-	public String indexList() {
+	public String indexList(Model model) {
+		List<IndexDto> list = indexService.getIndexList();			
+		model.addAttribute("list", list);
 		return "dbObject/index/indexList";
+	}
+	
+	@ResponseBody
+	@GetMapping("/searchIndex")
+	public List<IndexDto> searchIndex(
+			@RequestParam SchemaEnums schemaName,
+			@RequestParam String indexName
+			) {
+		List<IndexDto> list = new ArrayList<>();
+		if (!indexName.isEmpty()) {
+			list = indexService.getIndexList(schemaName, indexName);
+		} else {
+			list = indexService.getIndexList(schemaName);			
+		}
+		log.info("찾아줘" + list);
+		return list;
 	}
 	
 	@GetMapping("/indexAddForm")
--- src/main/webapp/resources/js/dbObject/index/indexList.js
+++ src/main/webapp/resources/js/dbObject/index/indexList.js
@@ -4,4 +4,42 @@ $(document).ready(function() {
     $('.sub-menu:eq(1)').addClass('active');
     $('.sub-menu:eq(1) .sub-item').removeClass('active');
     $('.sub-menu:eq(1) .sub-item:eq(2)').addClass('active');
+    
+	$(document).on('click', '#biSearch', function() { // $(document).on 추가
+
+		const schemaName = $('#schemaSelect').val();
+		const indexName = $('#indexNameSearch').val();
+		console.log(schemaName);
+		console.log(typeof schemaName);
+		console.log(indexName);
+		console.log(typeof indexName);
+
+		$.ajax({
+			type : 'GET',
+			url : 'searchIndex',
+			data : {
+				schemaName : schemaName,
+				indexName : indexName
+			},
+			success : function(data) {
+				console.log('성공?: ');
+				 let html = '';
+		            data.forEach(function(index) {
+		                html += `
+		                    <tr>
+		                        <th></th>
+		                        <td>${index.indexName}</td>
+		                        <td>${index.schemaName}</td>
+		                        <td>${index.tableName}</td>
+		                        <td>${index.columnName}</td>
+		                        <td>${index.uniqueness}</td>
+		                    </tr>`;
+		            });
+	            $('#indexTableBody').html(html);
+			},
+			error : function(xhr, status, error) {
+				console.log('오류: ' + xhr.responseText);
+			}
+		});
+	});
 });
