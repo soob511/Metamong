1c5f9a63af6003385cb27c856c169fecd275d1b3
PARK JEEAE <msp827@naver.com>
2024-10-29 16:09:45 +0900

    Rename: 폴더이동

--- src/main/java/com/mycompany/metamong/service/TeamService.java
+++ src/main/java/com/mycompany/metamong/service/TeamService.java
@@ -1,10 +1,11 @@
-package com.mycompany.metamong.daoMain;
+package com.mycompany.metamong.service;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mycompany.metamong.daoMain.TeamDao;
 import com.mycompany.metamong.dto.team.TeamDto;
 
 @Service
