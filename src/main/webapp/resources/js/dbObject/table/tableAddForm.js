0560174b93ba5877f671321f2aa0ba7fe8c70321
soob511 <asdfls@naver.com>
2024-10-29 10:00:56 +0900

    Feat: 컬럼선택후 수정가능

--- src/main/webapp/resources/js/dbObject/table/tableAddForm.js
+++ src/main/webapp/resources/js/dbObject/table/tableAddForm.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
         if (colNm === "" || colId === "" || colLength === "") {
         	Swal.fire({
         		  icon: 'warning',                  
-        		  title: '내용을 전부 입력해주세요.',    
+        		  title: '추가할 내용을 전부 입력해주세요.',    
         		});
         }else{
             var rowCount = $("#columnList tr").length + 1;
@@ -155,7 +155,19 @@ $(document).ready(function() {
     $(document).on("click", "#columnList tr", function () {
         $("#columnList tr").removeClass("selected");
         $(this).addClass("selected");
-        console.log(this);
+        const colNm = $(this).find("td:eq(1)").text();
+        const colId = $(this).find("td:eq(2)").text();
+        const dataType = $(this).find("td:eq(3)").text();
+        const colLength = $(this).find("td:eq(4)").text();
+        const colNullable = $(this).find("td:eq(5)").text();
+        const colPk = $(this).find("td:eq(6)").text();
+
+        $("#colNm").val(colNm);
+        $("#colId").val(colId);
+        $("#dataType").val(dataType);
+        $("#dataLength").val(colLength);
+        $("#nullable").val(colNullable);
+        $("#isUse").val(colPk);
     });
 
     function updateRowNumbers() {
@@ -163,5 +175,39 @@ $(document).ready(function() {
             $(this).find("td:first").text(index + 1);
         });
     }
+    
+    $(".btn-update").on("click", function () {
+        var selectedRow = $("#columnList tr.selected");
+        if (selectedRow.length) {
+            var colNm = $("#colNm").val();
+            var colId = $("#colId").val();
+            var dataType = $("#dataType").val();
+            var colLength = $("#dataLength").val();
+            var colNullable = $("#nullable").val();
+            var colPk = $("#isUse").val();
+
+            if (colNm === "" || colId === "" || colLength === "") {
+                Swal.fire({
+                    icon: 'warning',
+                    title: '수정할 내용을 전부 입력해주세요.'
+                });
+            } else {
+                selectedRow.find("td:eq(1)").text(colNm);
+                selectedRow.find("td:eq(2)").text(colId);
+                selectedRow.find("td:eq(3)").text(dataType);
+                selectedRow.find("td:eq(4)").text(colLength);
+                selectedRow.find("td:eq(5)").text(colNullable);
+                selectedRow.find("td:eq(6)").text(colPk);
+
+                $("#itemForm")[0].reset();
+                selectedRow.removeClass("selected"); 
+            }
+        } else {
+            Swal.fire({
+                icon: 'warning',
+                title: '수정할 행을 선택해주세요.'
+            });
+        }
+    });
 
 });
