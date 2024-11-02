f951c3dbf5c37c55253e7548cebdfbfcc63c3b9f
gilju <giljurha93@gmail.com>
2024-11-02 15:59:26 +0900

    Fix: 오류로 인해 전체조회 처리 경로 별도로 생성

--- src/main/webapp/resources/js/dbObject/index/indexList.js
+++ src/main/webapp/resources/js/dbObject/index/indexList.js
@@ -13,6 +13,14 @@ $(document).ready(function() {
 		filterIndex()
 	});
 	
+	$(document).on('click', '#biSearch', function() {
+		filterIndex()
+	});
+	
+	$(document).on('click', '#indexSearchBtn', function() {
+		filterIndex()
+	});
+	
 	$('#indexNameSearch').on('keyup', function() {
 		if (event.key != "Enter") {
 			return;
@@ -26,41 +34,71 @@ $(document).ready(function() {
 function filterIndex() {
 	const schemaName = $('#schemaSelect').val();
 	const indexName = $('#indexNameSearch').val().toUpperCase();
-	if (schemaName == 'ALL') {
-		location.reload();
-		return;
-	}
 	console.log(schemaName);
-	$.ajax({
-		type : 'GET',
-		url : 'searchIndex',
-		data : {
-			schemaName : schemaName,
-			indexName : indexName
-		},
-		success : function(data) {
-			let html = '';
-			let count = 1;
-
-			if (Object.keys(data).length > 0) {
-		            data.forEach(function(index) {
-		                html += `
-		                    <tr>
-		                        <th>${count++}</th>
-		                        <td>${index.indexName}</td>
-		                        <td>${index.schemaName}</td>
-		                        <td>${index.tableName}</td>
-		                        <td>${index.columnName}</td>
-		                        <td>${index.uniqueness}</td>
-		                    </tr>`;
-		            });
-			} else {
-				html += '<th colspan="6">조건에 맞는 인덱스가 없습니다</th>'
+	if (schemaName == 'ALL') {
+		$.ajax({
+			type : 'GET',
+			url : 'searchIndexAll',
+			data : {
+				indexName : indexName
+			},
+			success : function(data) {
+				let html = '';
+				let count = 1;
+				console.log(data);
+				if (Object.keys(data).length > 0) {
+					data.forEach(function(index) {
+						html += `
+							<tr>
+							<th>${count++}</th>
+							<td>${index.indexName}</td>
+							<td>${index.schemaName}</td>
+							<td>${index.tableName}</td>
+							<td>${index.columnName}</td>
+							<td>${index.uniqueness}</td>
+							</tr>`;
+					});
+				} else {
+					html += '<th colspan="6">조건에 맞는 인덱스가 없습니다</th>'
+				}
+				$('#indexTableBody').html(html);			 
+			},
+			error : function(xhr, status, error) {
+				console.log('오류: ' + xhr.responseText);
 			}
-			$('#indexTableBody').html(html);			 
-		},
-		error : function(xhr, status, error) {
-			console.log('오류: ' + xhr.responseText);
-		}
-	});
+		});
+	} else {
+		$.ajax({
+			type : 'GET',
+			url : 'searchIndex',
+			data : {
+				schemaName : schemaName,
+				indexName : indexName
+			},
+			success : function(data) {
+				let html = '';
+				let count = 1;
+				
+				if (Object.keys(data).length > 0) {
+					data.forEach(function(index) {
+						html += `
+							<tr>
+							<th>${count++}</th>
+							<td>${index.indexName}</td>
+							<td>${index.schemaName}</td>
+							<td>${index.tableName}</td>
+							<td>${index.columnName}</td>
+							<td>${index.uniqueness}</td>
+							</tr>`;
+					});
+				} else {
+					html += '<th colspan="6">조건에 맞는 인덱스가 없습니다</th>'
+				}
+				$('#indexTableBody').html(html);			 
+			},
+			error : function(xhr, status, error) {
+				console.log('오류: ' + xhr.responseText);
+			}
+		});
+	}
 }
