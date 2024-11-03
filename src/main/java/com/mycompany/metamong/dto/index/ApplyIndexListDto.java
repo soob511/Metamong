157fd5b1df6da6d7c65df8013df72c16cd0e54b3
gilju <giljurha93@gmail.com>
2024-11-03 10:14:47 +0900

    Fix: ApplyIndexListDto MId 필드 삭제 후 MName 추가 및 mapper-config에 별칭 추가

--- src/main/java/com/mycompany/metamong/dto/index/ApplyIndexListDto.java
+++ src/main/java/com/mycompany/metamong/dto/index/ApplyIndexListDto.java
@@ -1,5 +1,6 @@
 package com.mycompany.metamong.dto.index;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -9,10 +10,16 @@ import lombok.Data;
 @Data
 public class ApplyIndexListDto {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private int applyNo;
 	private Date applyDate;
-	private String MId;
+	private String MName;
 	private String schemaName;
 	private String idxName;
 	private String applyObj;
 	private int approvalStatus;
+	
+	public String getFmtApplyDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return applyDate != null ? sdf.format(applyDate) : "";
+    }
 }
