2882ae48632243b4db8533fe44260643eaabb2b5
soob511 <asdfls@naver.com>
2024-11-07 11:57:36 +0900

    Feat: 시퀀스 enum받아오기

--- src/main/java/com/mycompany/metamong/controller/SequenceController.java
+++ src/main/java/com/mycompany/metamong/controller/SequenceController.java
@@ -1,15 +1,19 @@
 package com.mycompany.metamong.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.metamong.enums.SchemaEnum;
 
 @Controller
 @RequestMapping("/sequence")
 public class SequenceController {
 	
 	@GetMapping("/sequenceList")
-	public String sequenceList() {
+	public String sequenceList(Model model) {
+		model.addAttribute("schemaEnum", SchemaEnum.values());
 		return "dbObject/sequence/sequenceList";
 	}
 	
