cf7f276c48057cbbf070c27493c099d8896154e4
gilju <giljurha93@gmail.com>
2024-11-08 18:03:13 +0900

    Feat: 홈화면에 공지사항, 테이블, 컬럼 정보 불러오는 기능 추가

--- src/main/java/com/mycompany/metamong/controller/HomeController.java
+++ src/main/java/com/mycompany/metamong/controller/HomeController.java
@@ -1,16 +1,36 @@
 package com.mycompany.metamong.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.metamong.dto.Pager;
+import com.mycompany.metamong.dto.applyList.ApprovalStatusCountDto;
+import com.mycompany.metamong.dto.member.ApprovalStatusDto;
+import com.mycompany.metamong.dto.notice.NoticeDto;
+import com.mycompany.metamong.enums.SchemaEnum;
+import com.mycompany.metamong.service.ApplyService;
+import com.mycompany.metamong.service.MemberService;
+import com.mycompany.metamong.service.NoticeService;
+import com.mycompany.metamong.service.TableService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class HomeController {
-	
+	@Autowired
+	private NoticeService noticeService;
+	@Autowired
+	private MemberService memberService;
+	@Autowired
+	private ApplyService applyService;
 	
 	@RequestMapping("")
 	public String index() {
@@ -19,9 +39,22 @@ public class HomeController {
 
 	@Secured("ROLE_USER")
 	@GetMapping("/homeUser")
-	public String homeUser() {
+	public String homeUser(Model model, Authentication auth) {
+		String userName = memberService.getDbaNameById(auth.getName());
+		Pager pager = new Pager(8, 10, 10, 1); 
+		List<NoticeDto> noticeList = noticeService.getNoticeList(pager);
+		
+		model.addAttribute("pager", pager);
+		model.addAttribute("userName", userName);
+		model.addAttribute("schemaEnum", SchemaEnum.values());
+		model.addAttribute("noticeList", noticeList);
 		log.info("실행");
 		return "home/homeUser";
+	}
+	
+	@GetMapping("/countApprovalStatus")
+	public ApprovalStatusDto countApprovalStatus(Authentication auth) {
+		return applyService.countApprovalStatus(auth.getName());
 	}
 
 	@GetMapping("/homeDBA")
