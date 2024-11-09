8f71993f061e24c84bacf5c2d0138d719fc20976
gilju <giljurha93@gmail.com>
2024-11-10 02:56:23 +0900

    Feat: 활성화된 계정목록, 1년 가입현황 불러오는 기능 구현

---
+++ src/main/java/com/mycompany/metamong/dto/member/ApprovalMemberStatusDto.java
@@ -0,0 +1,9 @@
+package com.mycompany.metamong.dto.member;
+
+import lombok.Data;
+
+@Data
+public class ApprovalMemberStatusDto {
+	private int total;
+	private int MApprovalStatus;
+}
--- src/main/java/com/mycompany/metamong/service/MemberService.java
+++ src/main/java/com/mycompany/metamong/service/MemberService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.mycompany.metamong.daoMain.MemberDao;
 import com.mycompany.metamong.dto.Pager;
+import com.mycompany.metamong.dto.member.ApplyMemberDto;
+import com.mycompany.metamong.dto.member.ApprovalMemberStatusDto;
 import com.mycompany.metamong.dto.member.MemberDto;
 
 @Service
@@ -41,5 +43,21 @@ public class MemberService {
 	
 	public String getDbaNameById(String dbaId) {
 		return memberDao.selectDbaName(dbaId);
+	}
+	
+	public MemberDto getMember(String mId) {
+		return memberDao.selectByMId(mId);
+	}
+	
+	public List<MemberDto> getActiveMember(int rowNumber) {
+		return memberDao.selectActiveMember(rowNumber);
+	}
+	
+	public List<ApplyMemberDto> getApplyMember(String mRole) {
+		return memberDao.selectApplyMember(mRole);
+	}
+	
+	public List<ApprovalMemberStatusDto> getApprovalStatus() {
+		return memberDao.selectApprovalStatus();
 	}
 }
