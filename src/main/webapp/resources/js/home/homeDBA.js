34e1ad2dc0b63302f61791d5fac404230dcd069b
gilju <giljurha93@gmail.com>
2024-11-10 10:57:13 +0900

    Feat: 승인대기 신청현황 요청 처리하는 기능 구현

--- src/main/webapp/resources/js/home/homeDBA.js
+++ src/main/webapp/resources/js/home/homeDBA.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
     $('.menu-item').removeClass('active');
     $('.menu-item:first').addClass('active');
     
+    getApprovalStatus();
+    searchTable().then(function() {
+		let tableId = $("#tableList tr").data('value');
+	    showColumnList(tableId);
+	});
+	$("#schemaSelect").on("change", function() {
+        searchTable();
+    });
+});
+
+function getApprovalStatus() {
 	let ctx = document.getElementById('chart').getContext('2d');
 	let myChart = new Chart(ctx, {
 	    type: 'doughnut',
@@ -38,4 +49,99 @@ $(document).ready(function() {
 	          }
 	    }
 	});
-});
+	$.ajax({
+		url : "/Metamong/getDbObjApprovalStatus",
+		type : "GET",
+		success : function(data) {
+			
+			const chartData = [0, 0, 0, 0];
+
+	        const summarySpans = $(".summary-box .fw-bold");
+	        
+	        data.forEach(item => {
+	            if (item.applyObj === 'CODE') {
+	                chartData[0] = item.count;
+	                summarySpans.eq(0).text(item.count);
+	            } else if (item.applyObj === 'TABLE') {
+	                chartData[1] = item.count;
+	                summarySpans.eq(1).text(item.count);
+	            } else if (item.applyObj === 'SEQUENCE') {
+	                chartData[2] = item.count;
+	                summarySpans.eq(2).text(item.count);
+	            } else if (item.applyObj === 'INDEX') {
+	                chartData[3] = item.count;
+	                summarySpans.eq(3).text(item.count);
+	            }
+	        });
+
+	        myChart.data.datasets[0].data = chartData;
+	        myChart.options.plugins.title.text = `전체 ${chartData.reduce((a, b) => a + b)}건`;
+	        myChart.update();
+		},
+		error : function(xhr, status, error) {
+			console.log('오류: ' + xhr.responseText);
+		}
+	});
+}
+
+function showColumnList(tableId) {
+    $.ajax({
+        url: "/Metamong/column/columnList",      
+        type: "GET",
+        data: { tableId: tableId },   
+        success: function(data) {
+        	var html=" ";
+        	var count = 0;
+            data.forEach(column => {
+                html += `<tr>
+                			<td>${++count}</td>
+                            <td>${column.colNm}</td>
+                			<td>${column.colId}</td>
+                            <td>${column.dataType}</td>
+                            <td>${column.colLength}</td>
+                            <td>${column.colIsnullable==0 ? 'NOT NULL' : 'NULL'}</td>
+                            <td>${column.colIspk==1 ? 'Y' : 'N'}</td>
+                        </tr>`;
+            });
+
+            $('#columnList').html(html);
+        }
+    });
+}
+
+function searchTable() {
+    var content = '';
+    var schema= $("#schemaSelect").val();
+
+    return $.ajax({
+        url: "/Metamong/table/tableSearch",
+        type: "GET",
+        data: { content: content,
+        		schema:schema
+        		},
+        success: function(data) {
+        	var html = "";
+        	var cHtml=`<th colspan="7">테이블을 선택해 주세요.</th>`; 
+        	if(Object.keys(data).length>0){
+        		var count = 0;
+        		data.forEach(table => {
+        			html += `
+	        			<tr data-value="${table.tableNo}" 
+	        			onclick="showColumnList(${table.tableNo})">
+        				<td>${++count}</td>
+        				<td>${table.tableId}</td>
+        				<td>${table.tableNm}</td>
+        				<td>${table.schemaNm}</td>
+        				</tr>`;
+        		});
+        	}else{
+        		html+= `<tr>
+    				<th colspan="5">해당 조건에 맞는 테이블이 없습니다.</th>
+    		
+    				</tr>`;
+        	}
+            $('#tableList').html(html);
+            $('#columnList').html(cHtml);
+        }
+    });
+}
