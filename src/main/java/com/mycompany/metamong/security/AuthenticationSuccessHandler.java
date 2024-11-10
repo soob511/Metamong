3ee3c8031f9b61d912259713762d9a1c48572ff6
gilju <giljurha93@gmail.com>
2024-11-10 13:20:06 +0900

    Feat: role에 맞게 해당 메인페이지로 이동하는 기능 구현

--- src/main/java/com/mycompany/metamong/security/AuthenticationSuccessHandler.java
+++ src/main/java/com/mycompany/metamong/security/AuthenticationSuccessHandler.java
@@ -1,19 +1,39 @@
 package com.mycompany.metamong.security;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws ServletException, IOException {
-		setDefaultTargetUrl("/homeUser");
-		super.onAuthenticationSuccess(request, response, authentication);
+
+		MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = memberDetails.getAuthorities();
+        
+        for (GrantedAuthority authority : authorities) {
+            if (authority.getAuthority().equals("ROLE_ADMIN")) {
+                response.sendRedirect("/Metamong/homeAdmin");
+                return;
+            } else if (authority.getAuthority().equals("ROLE_DBA")) {
+                response.sendRedirect("/Metamong/homeDBA");
+                return;
+            } else if (authority.getAuthority().equals("ROLE_USER")) {
+                response.sendRedirect("/Metamong/homeUser");
+                return;
+            }
+        }
+        
 	}
 }
