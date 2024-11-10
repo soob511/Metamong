1946db76b733a9282299635776b4816c74a3287c
soob511 <asdfls@naver.com>
2024-11-10 20:08:52 +0900

    Feat: 설정추가

--- src/main/java/com/mycompany/metamong/controller/AlarmController.java
+++ src/main/java/com/mycompany/metamong/controller/AlarmController.java
@@ -1,14 +1,16 @@
 package com.mycompany.metamong.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
--- src/main/java/com/mycompany/metamong/webSocket/WebSocketConfig.java
+++ src/main/java/com/mycompany/metamong/webSocket/WebSocketConfig.java
@@ -1,7 +1,15 @@
 package com.mycompany.metamong.webSocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -24,4 +32,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		registry.addEndpoint("/ws") // 클라이언트에서 접속할 엔드포인트
 				.setAllowedOriginPatterns("*").withSockJS(); // SockJS fallback 옵션
 	}
+
+	public void configureWebSocketTransport() {
+		ChannelInterceptor interceptor = new ChannelInterceptor() {
+			@Override
+			public Message<?> preSend(Message<?> message, MessageChannel channel) {
+				StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+				if (accessor != null && accessor.getUser() == null) {
+					Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+					if (auth != null) {
+						accessor.setUser(auth);
+					}
+				}
+				return message;
+			}
+		};
+	}
+
 }
