a95ab83a58ca3e7036805a3116bc82149600cb72
soob511 <asdfls@naver.com>
2024-11-11 23:07:20 +0900

    Feat: 데이터 타입 추가 기능

---
+++ src/main/java/com/mycompany/metamong/dto/dataType/dataTypeAddDto.java
@@ -0,0 +1,10 @@
+package com.mycompany.metamong.dto.dataType;
+
+import lombok.Data;
+
+@Data
+public class dataTypeAddDto {
+	private String dataType;
+	private int datatypeIsactive;
+	private int isUpdate;
+}
