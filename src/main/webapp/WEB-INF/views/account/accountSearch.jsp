8273ec1a38e239df94b6499c4d951164ce96c5e6
yoony <backyang0716@gmail.com>
2024-11-24 21:20:59 +0900

    fix: 팀리스트 selectbox 불러오도록 수정
    팀리스트대로 소속 selectbox 불러오도록 수정했습니다.

--- src/main/webapp/WEB-INF/views/account/accountSearch.jsp
+++ src/main/webapp/WEB-INF/views/account/accountSearch.jsp
@@ -36,13 +36,13 @@
 							<c:if test="${totalRows>=1}">
 								<c:forEach items="${list}"  var="member"  varStatus="status" >
 								<tr class="table-row">
-								<form id="updateAccount-${member.MId} " action="updateAccount" method="post">
+								<form id="updateAccount-${member.MId}" action="updateAccount" method="post">
 									<td scope="row">${pager.totalRows - (pager.pageNo-1) * 10 - status.index}</td>
-									<td id="mId-${member.MId}">${member.MId}</td>
-									<td id="mName-${member.MId}">${member.MName}</td>
+									<td id="mId-${member.MId}" ><input type="hidden" name="mId" value="${member.MId}" />${member.MId}</td>
+									<td id="mName-${member.MId}"><input type="hidden" name="mName" value="${member.MName}" />${member.MName}</td>
 									<td>
 										<div>
-										<select id="mRole-${member.MId}" class="form-select"
+										<select id="mRole-${member.MId}" name="mRole" class="form-select"
 											aria-label="Default select example">
 											<option value="ROLE_USER" ${member.MRole == 'USER' ? 'selected' : ''}>USER</option>
 											<option value="ROLE_DBA" ${member.MRole == 'DBA' ? 'selected' : ''}>DBA</option>
@@ -51,31 +51,25 @@
 										</div>
 									</td>
 									<td>
-										<div>
-											<select id="teamName-${member.MId}" class="form-select"
-												aria-label="Default select example">
-												<option value="개발1팀" ${member.teamName == '개발1팀' ? 'selected' : ''}>개발1팀</option>
-												<option value="개발2팀" ${member.teamName == '개발2팀' ? 'selected' : ''}>개발2팀</option>
-												<option value="개발3팀" ${member.teamName == '개발3팀' ? 'selected' : ''}>개발3팀</option>
-												<option value="개발4팀" ${member.teamName == '개발4팀' ? 'selected' : ''}>개발4팀</option>
-												<option value="개발5팀" ${member.teamName == '개발5팀' ? 'selected' : ''}>개발5팀</option>
-											</select>
+											<div>
+												  <select id="teamName-${member.MId}" class="form-select teamId" name="teamName"
+												    data-team-name="${member.teamName}" aria-label="Team Selection" value="${member.teamName}">
+												  </select>
+												</div>
+										</td>
+										<td id="edit"><input type="text" class="form-control" id="mEmpId-${member.MId}" name="mEmpId" value="${member.MEmpId}"></td>
+										<td id="edit"><input type="text" class="form-control" id="mTel-${member.MId}" name="mTel" value="${member.MTel}"></td>
+										<td><fmt:formatDate value="${member.MRegdate}" pattern="yyyy-MM-dd"/></td>
+										<td>
+											<div>
+												<select id="mIsactive-${member.MId}" class="form-select" name="mIsactive" aria-label="Default select example">
+													<option value="1" ${member.MIsActive == 1 ? 'selected' : ''}>Y</option>
+													<option value="0" ${member.MIsActive == 0 ? 'selected' : ''}>N</option>
+												</select>
 											</div>
 										</td>
-									<td id="edit"><input type="text" class="form-control"  id="mEmpId-${member.MId}" value="${member.MEmpId}"></td>
-									<td id="edit"><input type="text" class="form-control"  id="mTel-${member.MId}"  value="${member.MTel}"> </td>
-									<td><fmt:formatDate  value="${member.MRegdate}" pattern="yyyy-MM-dd"/></td>
-									<td>
-										<div>
-											<select id="mIsactive-${member.MId}" class="form-select"
-												aria-label="Default select example">
-												<option value="1" ${member.MIsActive == 1 ? 'selected' : ''}>Y</option>
-												<option value="0" ${member.MIsActive == 0 ? 'selected' : ''}>N</option>
-	
-											</select>
-										</div>
-									</td>
-									<td><button class="btn btn-sm me-2 btn-approve"  id="btn-approve" data-m-id="${member.MId}" type="button">수정</button></td>
+										<td><button class="btn btn-sm me-2 btn-approve" id="btn-approve" data-m-id="${member.MId}" type="button">수정</button></td>
+										<input type="hidden" name="mId" value="${member.MId}">					
 									</form>
 								</tr>
 							</c:forEach>
--- src/main/webapp/resources/js/account/accountList.js
+++ src/main/webapp/resources/js/account/accountList.js
@@ -1,4 +1,3 @@
-let num = $(".btn-team").data("team-length");
 let teams = [];
 let teamIndex, updateTeam, teamId;
 
@@ -227,59 +226,148 @@ function accountSearch(pageNo) {
     data: { option: option, keyword: keyword, pageNo: pageNo },
     success: function (data) {
       $("#memberList").html(data);
+      getTeamList();
     },
   });
 }
 
-// 회원 정보 수정
 $(document).ready(function () {
-  $(".btn-approve").on("click", function () {
-    var mId = $(this).data("m-id");
-    var mRole = $(`#mRole-${mId}`).val();
-    var teamName = $(`#teamName-${mId}`).val();
-    var mEmpId = $(`#mEmpId-${mId}`).val();
-    var mTel = $(`#mTel-${mId}`).val();
-    var mIsactive = $(`#mIsactive-${mId} option:selected`).val();
+	  $("#memberList").on("click", ".btn-approve", function () {
+	    var mId = $(this).data("m-id");
+	    var mRole = $(`#mRole-${mId}`).val();
+	    var teamId = $(`#teamName-${mId}`).val();
+	    var teamName = $(`#teamName-${mId} option:selected`).text();
+	    var mEmpId = $(`#mEmpId-${mId}`).val();
+	    var mTel = $(`#mTel-${mId}`).val();
+	    var mIsactive = $(`#mIsactive-${mId} option:selected`).val();
 
-    var formData = new FormData();
-    formData.append("mId", mId);
-    formData.append("mRole", mRole);
-    formData.append("teamName", teamName);
-    formData.append("mEmpId", mEmpId);
-    formData.append("mTel", mTel);
-    formData.append("mIsactive", mIsactive);
+	    var formData = new FormData();
+	    formData.append("mId", mId);
+	    formData.append("mRole", mRole);
+	    formData.append("teamId", teamId);
+	    formData.append("teamName", teamName);
+	    formData.append("mEmpId", mEmpId);
+	    formData.append("mTel", mTel);
+	    formData.append("mIsactive", mIsactive);
 
-    if (!mEmpId || !mTel) {
-      Swal.fire({
-        icon: "warning",
-        title: "필수내역을 공란없이<br/>입력해 주세요.",
-        text: "필수입력사항: 사번, 연락처",
-      });
-      return;
-    }
+	    if (!mEmpId || !mTel) {
+	      Swal.fire({
+	        icon: "warning",
+	        title: "필수내역을 공란없이<br/>입력해 주세요.",
+	        text: "필수입력사항: 사번, 연락처",
+	      });
+	      return;
+	    }
+	    const params = new URLSearchParams(window.location.search);
+	    const option = params.get("option") || ""; 
+	    const keyword = params.get("keyword") || ""; 
+	    const pageNo = params.get("pageNo") || ""; 
 
-    const params = new URLSearchParams(window.location.search);
-    const pageNo = params.get("pageNo");
+	    $.ajax({
+	      url: "/Metamong/account/updateAccount",
+	      type: "POST",
+	      data: formData,
+	      processData: false,
+	      contentType: false,
+	      success: function (data) {
+	        Swal.fire({
+	          icon: "success",
+	          title: "회원정보 수정이 완료되었습니다.",
+	          text: "수정된 사항을 확인해주세요.",
+	        }).then((result) => {
+	          location.href = `/Metamong/account/accountList?option=${option}&keyword=${keyword}&pageNo=${pageNo}`;
+	        });
+	      },
+	    });
+	  });
+	});
 
-    $.ajax({
-      url: "/Metamong/account/updateAccount",
-      type: "POST",
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function (data) {
-        Swal.fire({
-          icon: "success",
-          title: "회원정보 수정이 완료되었습니다.",
-          text: "수정된 사항을 확인해주세요.",
-        }).then((result) => {
-          if (pageNo) {
-            location.href = `/Metamong/account/accountList?pageNo=${pageNo}`;
-          } else {
-            location.href = `/Metamong/account/accountList`;
-          }
-        });
-      },
-    });
-  });
+// 회원 정보 수정
+
+	function getTeamList(callback) {
+	$(".teamId").each(function () {
+	    const $select = $(this);
+	    const memberTeamName = $select.data("team-name"); 
+
+	    $.ajax({
+	      url: "/Metamong/team/getTeamList",
+	      type: "GET",
+	      dataType: "json",
+	      success: function (data) {
+	        //$select.empty(); 
+	        data.forEach((team) => {
+	        	$select.append(
+	        			  `<option value="${team.teamId}" ${
+	        			    team.teamName == memberTeamName ? "selected" : ""
+	        			  }>${team.teamName}</option>`
+	        			);
+
+	        });
+	      },
+	      error: function (jqXHR, textStatus, errorThrown) {
+	        console.error("Error fetching team list:", textStatus, errorThrown);
+	      },
+	    });
+	  });
+	}
+	
+	$(document).ready(function () {
+		  getTeamList(function () {
+		    // 팀 리스트 처리 완료 후 계정 검색 실행
+		    accountSearch(1);
+		  });
+		});
+
+	$(document).ready(function () {
+		$(".btn-approve").on("click", function () {
+			  var mId = $(this).data("m-id");
+			  var mRole = $(`#mRole-${mId}`).val();
+			  var teamId = $(`#teamName-${mId}`).val(); 
+			  var teamName = $(`#teamName-${mId} option:selected`).text(); 
+			  var mEmpId = $(`#mEmpId-${mId}`).val();
+			  var mTel = $(`#mTel-${mId}`).val();
+			  var mIsactive = $(`#mIsactive-${mId} option:selected`).val();
+
+			  var formData = new FormData();
+			  formData.append("mId", mId);
+			  formData.append("mRole", mRole);
+			  formData.append("teamId", teamId); 
+			  formData.append("teamName", teamName); 
+			  formData.append("mEmpId", mEmpId);
+			  formData.append("mTel", mTel);
+			  formData.append("mIsactive", mIsactive);
+
+			  if (!mEmpId || !mTel) {
+			    Swal.fire({
+			      icon: "warning",
+			      title: "필수내역을 공란없이<br/>입력해 주세요.",
+			      text: "필수입력사항: 사번, 연락처",
+			    });
+			    return;
+			  }
+
+			  const params = new URLSearchParams(window.location.search);
+			  const pageNo = params.get("pageNo");
+
+			  $.ajax({
+			    url: "/Metamong/account/updateAccount",
+			    type: "POST",
+			    data: formData,
+			    processData: false,
+			    contentType: false,
+			    success: function (data) {
+			      Swal.fire({
+			        icon: "success",
+			        title: "회원정보 수정이 완료되었습니다.",
+			        text: "수정된 사항을 확인해주세요.",
+			      }).then((result) => {
+			        if (pageNo) {
+			          location.href = `/Metamong/account/accountList?pageNo=${pageNo}`;
+			        } else {
+			          location.href = `/Metamong/account/accountList`;
+			        }
+			      });
+			    },
+			  });
+			});
 });
